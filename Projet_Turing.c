635480cf76d0e600d7f16843f7e7a3796a3d5ec4
BruhP8 <luc.biohazard@gmail.com>
2018-10-31 13:24:32 +0100

    Lecture de fichier

--- Projet_Turing.c
+++ Projet_Turing.c
@@ -1,14 +1,20 @@
 //                        ****** Zone Nicolas ******                        //
+#include<stdio.h>
+#include<stdlib.h>
+#define TAILLEMAX 10000
 
-
-
-
-
-
-
-
-
-
+void init (char * file_name){
+	FILE * file;
+	file = fopen(file_name, "r"); //charge le fichier
+	if (file == NULL){
+		printf("Error loading the file %s . Does it exist ?\n", file_name); //fichier pas chargé...
+	}else {
+		char init_tape[TAILLEMAX];
+		fgets(init_tape, TAILLEMAX, file); //lis la première ligne et la stocke dans init_tape
+		printf("%s \n", init_tape);
+	}
+	
+}
 
 
 
@@ -35,3 +41,10 @@
 
 
 //                        ****** Main ******                        //
+
+
+
+int main (int argc, char *argv[]){
+	init(argv[1]);
+	return 0;
+}
