249f38cb0b512b3c0fa06e0a7f4cf66f968da286
BruhP8 <luc.biohazard@gmail.com>
2018-10-31 19:39:50 +0100

    Tentative de convertisseur ascii binaire

--- Projet_Turing.c
+++ Projet_Turing.c
@@ -3,21 +3,39 @@
 #include<stdlib.h>
 #define TAILLEMAX 10000
 
+int binary_convertor (char c){
+	int binary = 0, i = 1, remainder;
+	while (c > 0){
+		remainder = c % 2;
+		binary = binary + (i * remainder);
+		c = c / 2;
+		i = i *10;
+	}
+	return binary;
+}
+
 void init (char * file_name){
 	FILE * file;
 	file = fopen(file_name, "r"); //charge le fichier
 	if (file == NULL){
 		printf("Error loading the file %s . Does it exist ?\n", file_name); //fichier pas chargé...
 	}else {
-		char init_tape[TAILLEMAX];
-		fgets(init_tape, TAILLEMAX, file); //lis la première ligne et la stocke dans init_tape
-		printf("%s \n", init_tape);
+		int init_tape[TAILLEMAX];
+		for (int i = 0; (fgetc(file) != EOF); i++){
+			init_tape[i] = binary_convertor(fgetc(file));
+			printf("%d \n", init_tape[i] );
+		}
+
+
+
+		//fgets(init_tape, TAILLEMAX, file); //lis la première ligne et la stocke dans init_tape
+		//printf("%s \n", init_tape);
 	}
 	
 }
 
 
-// attention deux fichiers .tm a passer : le ruban et les règles
+// attention deux fichiers .tm a passer : le ruban et les règles. Tout input DOIT ETRE BINAIRE -> convertir
 
 
 
