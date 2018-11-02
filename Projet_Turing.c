863b8fb9a5a44353d73fbe5f1c7071bc142deb24
BruhP8 <luc.biohazard@gmail.com>
2018-11-02 11:00:39 +0100

    taille

--- Projet_Turing.c
+++ Projet_Turing.c
@@ -3,35 +3,32 @@
 #include<stdlib.h>
 #define TAILLEMAX 10000
 
-int binary_convertor (char c){
-	int binary = 0, i = 1, remainder;
-	while (c > 0){
-		remainder = c % 2;
-		binary = binary + (i * remainder);
-		c = c / 2;
-		i = i *10;
-	}
-	return binary;
-}
 
-void init (char * file_name){
+char * init (char * file_name){
 	FILE * file;
 	file = fopen(file_name, "r"); //charge le fichier
 	if (file == NULL){
 		printf("Error loading the file %s . Does it exist ?\n", file_name); //fichier pas chargé...
 	}else {
-		char init_tape[TAILLEMAX];
+		static char init_tape[10];
 		printf("Initial State is : ");
 		for (int i = 0; (fgetc(file) != EOF); i++){
 			init_tape[i] = fgetc(file);
 			printf("%c ", init_tape[i] );
 		}
 		printf("\n");
+		return init_tape;
 	}
-	
+}	
+
+char * size_increase (char * init_tape){
+	static char tmp[sizeof(init_tape)/sizeof(char) * 2];
+	for (int i = 0; i<(sizeof(init_tape)/sizeof(char)); i++){
+		tmp[i] = init_tape[i];
+		
+	}
+	return tmp;
 }
-
-
 // attention deux fichiers .tm a passer : le ruban et les règles. Tout input DOIT ETRE BINAIRE -> convertir
 
 
@@ -57,6 +54,10 @@ void init (char * file_name){
 
 
 int main (int argc, char *argv[]){
-	init(argv[1]);
+	char *init_tape; 
+	int head_pos = 0;
+	init_tape = init(argv[1]);
+	size_increase(init_tape);
+	printf("%c \n", init_tape[1]);
 	return 0;
 }	
